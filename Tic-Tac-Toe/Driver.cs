dfa79f333909801adaa2a81577817b13430e8226
Cade Loar <cade.loar@gmail.com>
2023-01-23 13:05:49 -0700

    added instructions

--- Tic-Tac-Toe/Driver.cs
+++ Tic-Tac-Toe/Driver.cs
@@ -4,6 +4,13 @@ using System.Text;
 
 namespace Tic_Tac_Toe
 {
+    // Welcome the user to the game
+    // Create a game board array to store the players’ choices
+    // Ask each player in turn for their choice and update the game board array
+    // Print the board by calling the method in the supporting class
+    // Check for a winner by calling the method in the supporting class, and notify the players
+    // when a win has occurred and which player won the game
+
     class Driver
     {
     }
