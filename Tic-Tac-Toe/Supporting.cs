b5bcf8fd984197ca1da389ed94d34d68ec6128f2
Riley Marshall <rileyrm@byu.edu>
2023-01-23 13:46:00 -0700

    Made the display board method

--- Tic-Tac-Toe/Supporting.cs
+++ Tic-Tac-Toe/Supporting.cs
@@ -7,5 +7,19 @@ namespace Tic_Tac_Toe
     class Supporting
     {
         //Just making a comment to test the git features
+        
+        //Display Board method, intakes a 2d array
+        public void DisplayBoard(char[,] array)
+        {
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  " + array[0, 0] + "  |  " + array[0, 1] + "  |  " + array[0, 2]);
+            Console.WriteLine("_____|_____|_____ ");
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  " + array[1, 0] + "  |  " + array[1, 1] + "  |  " + array[1, 2]);
+            Console.WriteLine("_____|_____|_____ ");
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  " + array[2, 0] + "  |  " + array[2, 1] + "  |  " + array[2, 2]);
+            Console.WriteLine("     |     |      ");
+        }
     }
 }
