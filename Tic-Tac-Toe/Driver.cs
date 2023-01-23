d3541682cba4e11409e66354ce42512c84e6d545
Brandon Jacobs <brandon.jacobs345@gmail.com>
2023-01-23 15:39:50 -0700

    Two Player Game

--- Tic-Tac-Toe/Driver.cs
+++ Tic-Tac-Toe/Driver.cs
@@ -13,5 +13,85 @@ namespace Tic_Tac_Toe
 
     class Driver
     {
+        public char[,] gameBoard = new char[3, 3] { { '1', '2', '3' }, { '4', '5', '6' }, { '7', '8', '9' } };
+        public int counter = 0;
+
+        int[] position = new int[2];
+
+        public Dictionary<int, int[]> positionLookup = new Dictionary<int, int[]>()
+        {
+            {1, new int[] {0,0} },
+            {2, new int[] {0,1} },
+            {3, new int[] {0,2} },
+            {4, new int[] {1,0} },
+            {5, new int[] {1,1} },
+            {6, new int[] {1,2} },
+            {7, new int[] {2,0} },
+            {8, new int[] {2,1} },
+            {9, new int[] {2,2} }
+
+        };
+        public void PlayGame()
+        {
+            Console.WriteLine("Welcome to the game!");
+
+            int i = 0;
+            while (!checkWinner() | i < 9)
+            {
+                IterateGame();
+                PrintBoard();
+                checkWinner();
+                i++;
+            }
+            
+        } 
+        public int userInput()
+        {
+            Console.Write("Where do you want to place your guess (Enter 0-9)? ");
+            int userGuess =  Int32.Parse(Console.ReadLine());
+
+            return userGuess;
+        }
+        public void IterateGame()
+        {
+            int userGuess = userInput();
+            
+            int[] arr;
+            arr = positionLookup[userGuess];
+            int x = arr[0];
+            int y = arr[1];
+
+            counter++;
+            if (counter % 2 == 1)
+            {
+                gameBoard[x, y] = 'o';
+            }
+            else
+            {
+                gameBoard[x, y] = 'x';
+            }
+            
+        }
+        public void PrintBoard()
+        {
+            //Call supporting class
+        }
+        public bool checkWinner()
+        {
+            bool isWinner = //call supporitng class;
+            if (isWinner)
+            {
+                if(counter % 2 == 1)
+                {
+                    Console.WriteLine("Player 1 won!");
+                }
+                else
+                {
+                    Console.WriteLine("Player 2 won!");
+                }
+            }
+            return isWinner;
+        }
+
     }
 }
--- Tic-Tac-Toe/Program.cs
+++ Tic-Tac-Toe/Program.cs
@@ -6,7 +6,24 @@ namespace Tic_Tac_Toe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Driver game = new Driver();
+            game.PlayGame();
+            
+            
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(game.gameBoard[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+
+
+
+
+
         }
     }
 }
